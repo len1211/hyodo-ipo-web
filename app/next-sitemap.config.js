dc17fd55b093846a741fd7958b416df234e7c1bb
len1211 <len1211@naver.com>
2025-11-21 20:36:48 +0900

    Add sitemap

---
+++ app/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://hyodo-care.com', // 승환님의 실제 도메인
+    generateRobotsTxt: true, // robots.txt 파일도 같이 만들어줌
+    sitemapSize: 7000,
+  }
